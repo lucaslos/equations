0e88bca1fd9626fa5387479d56c9ba9ae290ab54
lucaslos <lucas@sugestly.com>
2014-08-27 11:10:56 -0300

    Update style

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function addEvents() {
     var labels = document.getElementsByName("label"),
+        imgs = document.getElementsByName("img"),
         length = labels.length;
     
     for(i = 0; i<length; i++){
         labels[i].addEventListener("click", clickLabel, false);
+        imgs[i].addEventListener("click", clickLabel, false);
     }
 }
 
@@ -13,13 +15,17 @@ function clickLabel() {
     
     if(this.getAttribute("data-open") == 0 || this.getAttribute("data-open") == null){
         this.setAttribute("data-open","1");
-        this.setAttribute("style","-webkit-transform: translateX(90%);");
-        this.children[0].setAttribute("style","-webkit-transform: rotate(180deg);")
-        this.children[1].style.opacity = "0";
+        this.setAttribute("style","-webkit-transform: rotateX(180deg); box-shadow: none;");
+        
+        this.parentNode.children[1].setAttribute("data-open","1");
+        this.parentNode.children[1].setAttribute("style","-webkit-transform: rotateX(0deg); box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26), 0 2px 10px 0 rgba(0, 0, 0, 0.16);");
+        
     } else {
         this.setAttribute("data-open","0");
-        this.setAttribute("style","");
-        this.children[0].setAttribute("style","");
-        this.children[1].style.opacity = "1";
+        this.setAttribute("style","-webkit-transform: rotateX(-180deg); box-shadow: none;");
+        
+        this.parentNode.children[0].setAttribute("data-open","0");
+        this.parentNode.children[0].setAttribute("style","-webkit-transform: rotateX(0deg); box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26), 0 2px 10px 0 rgba(0, 0, 0, 0.16);");
+        
     }
 }
